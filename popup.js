a2da56b18f3efafae0abb9af632ea2352b2cb7b9
Suraj <2015suraj.pawar@ves.ac.in>
2020-09-10 17:04:27 -0400

    add navigation

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('upload-resume');
-    checkPageButton.addEventListener('click', function() {
-  
-      });
-    }, false);
+document.addEventListener('DOMContentLoaded', function () {
+	var checkPageButton = document.getElementById('upload-resume');
+	checkPageButton.addEventListener('click', function () {
+
+	});
+}, false);
+
