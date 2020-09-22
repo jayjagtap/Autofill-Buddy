93dec22503dfcdb567b7cbc46aeaafc197c2db05
patel97 <patelamit791@gmail.com>
2020-09-21 23:29:23 -0400

    Hooked js tests

--- Code/newProfile.js
+++ Code/newProfile.js
@@ -26,34 +26,6 @@ window.onload = function () {
   if (addWorkExButton) {
     addWorkExButton.addEventListener('click', addNewWorkEx)
   }
-  function addNewWorkEx () {
-    var WE = document.getElementById('WE')
-    var divEntry = document.createElement('div')
-    divEntry.className = 'wex'
-    var newWE = document.getElementsByClassName('wex')[0]
-    divEntry.innerHTML = newWE.innerHTML
-    WE.appendChild(divEntry)
-    assignIdsWorkEx()
-  }
-
-  function assignIdsWorkEx () {
-    var workexs = document.getElementsByClassName('wex')
-    var delworkexs = document.getElementsByClassName('delWex')
-    console.log(workexs.length)
-    for (var i = 0; i < workexs.length; i++) {
-      workexs[i].id = 'workexperience' + (i + 1)
-      delworkexs[i].id = 'delworkex' + (i + 1)
-      document.getElementById('delworkex' + (i + 1)).addEventListener('click', function () {
-        var delID = this.parentNode.id
-        if (delID !== 'workexperience1') {
-          var entry = document.getElementById(delID)
-          console.log(entry)
-          entry.remove()
-        }
-        assignIdsWorkEx()
-      })
-    }
-  }
 
   var addEducationButton = document.getElementById('addEdu')
   if (addEducationButton) {
@@ -120,3 +92,33 @@ window.onload = function () {
     location.href = 'popup.html'
   })
 }
+
+function assignIdsWorkEx () {
+  var workexs = document.getElementsByClassName('wex')
+  var delworkexs = document.getElementsByClassName('delWex')
+  for (var i = 0; i < workexs.length; i++) {
+    workexs[i].id = 'workexperience' + (i + 1)
+    delworkexs[i].id = 'delworkex' + (i + 1)
+    document.getElementById('delworkex' + (i + 1)).addEventListener('click', function () {
+      var delID = this.parentNode.id
+      if (delID !== 'workexperience1') {
+        var entry = document.getElementById(delID)
+        console.log(entry)
+        entry.remove()
+      }
+      assignIdsWorkEx()
+    })
+  }
+}
+
+function addNewWorkEx () {
+  var WE = document.getElementById('WE')
+  var divEntry = document.createElement('div')
+  divEntry.className = 'wex'
+  var newWE = document.getElementsByClassName('wex')[0]
+  divEntry.innerHTML = newWE.innerHTML
+  WE.appendChild(divEntry)
+  assignIdsWorkEx()
+}
+
+module.exports = {addNewWorkEx}
---
+++ Code/test/newProfile.test.js
@@ -0,0 +1,52 @@
+// __tests__/displayUser-test.js
+/* global expect */
+'use strict'
+const {addNewWorkEx} = require('../newProfile')
+
+test('Add Work Experience', () => {
+  // Set up our document body
+//   document.head.innerHTML = '<script src="newProfile.js"></script>'
+  document.body.innerHTML =
+//   '<script src="newProfile.js"></script>'+
+  '<div id="WE">'+
+  '<div class="wex" id="workexperience">'+
+      '<table>'+
+          '<tr>'+
+              '<td><label for="jobtitle">Job Title:</label></td>'+
+              '<td><input type="text" id="jobtitle" name="jobtitle"></td>'+
+          '</tr>'+
+          '<tr>'+
+              '<td><label for="company">Company:</label></td>'+
+             ' <td><input type="text" id="Company" name="Company"></td>'+
+          '</tr>'+
+          '<tr>'+
+              '<td><label for="location">Location:</label></td>'+
+              '<td><input type="text" id="wlocation" name="wlocation"></td>'+
+          '</tr>'+
+          '<tr>'+
+              '<td><label for="fromDate">From Date:</label></td>'+
+              '<td><input type="month" id="fromDate" name="fromDate" placeholder="YYYY-MM"></td>'+
+          '</tr>'+
+          '<tr>'+
+              '<td><label for="fromDate">To Date:</label></td>'+
+              '<td><input type="month" id="toDate" name="toDate" placeholder="YYYY-MM"></td>'+
+          '</tr>'+
+          '<tr>'+
+              '<td><label for="roledescription">Role Description:</label></td>'+
+              '<td><textarea id="roledescription" name="roledescription"></textarea><br /></td>'+
+          '</tr>'+
+      '</table>'+
+      '<input type="button" class="btn btn-danger delWex" value="Remove workex">'+
+      '<br/><br/>'+
+  '</div>'+
+'</div>'+
+'<br/>'+
+'<input type="button" id="addWorkEx" class="btn btn-info" value="Add new Work Ex" >'
+
+  addNewWorkEx()
+  addNewWorkEx()
+  addNewWorkEx()
+
+
+  expect(document.getElementsByClassName('wex').length).toEqual(4)
+})
